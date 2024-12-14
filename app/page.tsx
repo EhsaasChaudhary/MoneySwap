6115c942f39db4a27def8a056bc85b443020dc3a
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-12-14 19:54:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import CurrencyConverterCard from "./components/currencyconvertcard";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Boxes } from "@/components/ui/background-boxes";
-import { AnimatedCurrencyBackground } from "./components/animatedcurrencybackground";
+// import { AnimatedCurrencyBackground } from "./components/animatedcurrencybackground";
 
 
 export default function Home() {
   return (
     <div className="page-container bg-slate-900 ">
       <Header />
-      <AnimatedCurrencyBackground />
+      {/* <AnimatedCurrencyBackground /> */}
       <Boxes/>
       <div className="main-content">
         <CurrencyConverterCard />
